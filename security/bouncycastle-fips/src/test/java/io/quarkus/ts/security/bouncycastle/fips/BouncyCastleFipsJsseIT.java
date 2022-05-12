0fbb1af83ffaffed210c7635276e683498d8231f
Michal Vavřík <mvavrik@redhat.com>
2022-05-12 13:14:49 +0200

    use RestAssured

--- security/bouncycastle-fips/src/test/java/io/quarkus/ts/security/bouncycastle/fips/BouncyCastleFipsJsseIT.java
+++ security/bouncycastle-fips/src/test/java/io/quarkus/ts/security/bouncycastle/fips/BouncyCastleFipsJsseIT.java
@@ -1,37 +1,33 @@
 package io.quarkus.ts.security.bouncycastle.fips;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.FileInputStream;
 import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.SecureRandom;
 import java.security.Security;
 import java.util.List;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnJre;
-import org.junit.jupiter.api.condition.JRE;
 
-import io.quarkus.runtime.util.ClassPathUtils;
-import io.quarkus.test.bootstrap.Protocol;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.Dependency;
 import io.quarkus.test.services.QuarkusApplication;
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.net.KeyStoreOptions;
-import io.vertx.ext.web.client.WebClientOptions;
-import io.vertx.mutiny.ext.web.client.WebClient;
+import io.restassured.RestAssured;
+import io.restassured.config.SSLConfig;
 
 @QuarkusScenario
-// TODO https://github.com/quarkusio/quarkus/issues/25516
-@DisabledOnJre(value = JRE.JAVA_17)
 public class BouncyCastleFipsJsseIT {
 
     private static final String PASSWORD = "password";
@@ -45,63 +41,54 @@ public class BouncyCastleFipsJsseIT {
     private static final RestService app = new RestService().withProperties("jsse.properties");
 
     @Test
-    public void verifyBouncyCastleFipsAndJsseProviderAvailability() throws Exception {
-        Security.insertProviderAt(new BouncyCastleFipsProvider(), 1);
-        WebClient webClient = WebClient.create(new io.vertx.mutiny.core.Vertx(Vertx.vertx()), createWebClientOptions());
-        String endpoint = app.getHost(Protocol.HTTPS) + ":" + app.getPort(Protocol.HTTPS) + "/api/listProviders";
-        String body = webClient
-                .getAbs(endpoint)
-                .sendAndAwait().bodyAsString();
-
+    public void verifyBouncyCastleFipsAndJsseProviderAvailability() {
+        BouncyCastleFipsProvider provider = new BouncyCastleFipsProvider();
+        Security.insertProviderAt(provider, 1);
         String expectedResp = String.join(",", List.of(BCFIPS, BCJSSE));
-        assertThat(body, containsString(expectedResp));
+        given()
+                .config(RestAssured
+                        .config()
+                        .sslConfig(
+                                new SSLConfig()
+                                        .sslSocketFactory(sslSocketFactory(provider))))
+                .when()
+                .get("/api/listProviders")
+                .then()
+                .statusCode(200)
+                .body(containsString(expectedResp));
     }
 
-    private WebClientOptions createWebClientOptions() throws Exception {
-        WebClientOptions webClientOptions = new WebClientOptions();
+    private SSLSocketFactory sslSocketFactory(
+            BouncyCastleFipsProvider provider) {
 
-        byte[] keyStoreData = getFileContent(Paths.get("src", "test", "resources", "client-keystore.jks"));
-        KeyStoreOptions keyStoreOptions = new KeyStoreOptions()
-                .setPassword(PASSWORD)
-                .setValue(Buffer.buffer(keyStoreData))
-                .setType(KS_TYPE)
-                .setProvider(BCFIPS);
-        webClientOptions.setKeyCertOptions(keyStoreOptions);
+        SSLSocketFactory clientAuthFactory = null;
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KS_TYPE);
+            keyStore.load(new FileInputStream(Paths.get("src", "test", "resources", "client-keystore.jks").toFile()),
+                    PASSWORD.toCharArray());
 
-        byte[] trustStoreData = getFileContent(Paths.get("src", "test", "resources", "client-truststore.jks"));
-        KeyStoreOptions trustStoreOptions = new KeyStoreOptions()
-                .setPassword(PASSWORD)
-                .setValue(Buffer.buffer(trustStoreData))
-                .setType(KS_TYPE)
-                .setProvider(BCFIPS);
-        webClientOptions.setVerifyHost(false).setTrustAll(true).setTrustOptions(trustStoreOptions);
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(keyStore, PASSWORD.toCharArray());
+            KeyManager[] kms = kmf.getKeyManagers();
 
-        return webClientOptions;
-    }
+            KeyStore trustStore = KeyStore.getInstance(KS_TYPE);
+            keyStore.load(new FileInputStream(Paths.get("src", "test", "resources", "client-truststore.jks").toFile()),
+                    PASSWORD.toCharArray());
 
-    private static byte[] getFileContent(Path path) throws IOException {
-        byte[] data;
-        final InputStream resource = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream(ClassPathUtils.toResourceName(path));
-        if (resource != null) {
-            try (InputStream is = resource) {
-                data = doRead(is);
-            }
-        } else {
-            try (InputStream is = Files.newInputStream(path)) {
-                data = doRead(is);
-            }
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            //Adds the truststore to the factory
+            tmf.init(trustStore);
+            //This is passed to the SSLContext init method
+            TrustManager[] tms = tmf.getTrustManagers();
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kms, tms, SecureRandom.getInstance("DEFAULT", provider));
+
+            clientAuthFactory = new SSLSocketFactory(sslContext, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        } catch (Exception e) {
+            System.out.println("Error while loading keystore: " + e);
         }
-        return data;
+        return clientAuthFactory;
     }
 
-    private static byte[] doRead(InputStream is) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        byte[] buf = new byte[1024];
-        int r;
-        while ((r = is.read(buf)) > 0) {
-            out.write(buf, 0, r);
-        }
-        return out.toByteArray();
-    }
 }
