d2943f753fcc4931de6d493ff1392ae4a0b7c7c3
Michal Vavřík <mvavrik@redhat.com>
2022-11-06 11:19:19 +0100

    refactor

--- messaging/kafka-strimzi-avro-reactive-messaging/src/main/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/KStockPriceProducer.java
+++ messaging/kafka-strimzi-avro-reactive-messaging/src/main/java/io/quarkus/ts/messaging/strimzi/kafka/reactive/KStockPriceProducer.java
@@ -5,6 +5,7 @@ import java.util.function.BiConsumer;
 import java.util.stream.IntStream;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -13,16 +14,19 @@ import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.OnOverflow;
 import org.jboss.logging.Logger;
 
-import io.quarkus.scheduler.Scheduled;
+import io.quarkus.runtime.StartupEvent;
 import io.quarkus.ts.messaging.kafka.StockPrice;
 import io.quarkus.ts.messaging.kafka.status;
 import io.smallrye.common.constraint.NotNull;
+import io.vertx.core.Handler;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 @ApplicationScoped
 public class KStockPriceProducer {
 
     private static final Logger LOG = Logger.getLogger(KStockPriceProducer.class);
-    private static final int BATCH_SIZE = 100;
+    private static final int BATCH_SIZE = 1000;
 
     @Inject
     @Channel("source-stock-price")
@@ -32,20 +36,116 @@ public class KStockPriceProducer {
     @ConfigProperty(name = "cron.expr.skip", defaultValue = "false")
     String skipCronJob;
 
+    @Inject
+    Vertx vertx;
+
     private Random random = new Random();
 
-    @Scheduled(cron = "{cron.expr}")
-    public void generate() {
-        if (!Boolean.parseBoolean(skipCronJob)) {
-            IntStream.range(0, BATCH_SIZE).forEach(next -> {
-                StockPrice event = StockPrice.newBuilder()
-                        .setId("IBM")
-                        .setPrice(random.nextDouble())
-                        .setStatus(status.PENDING)
-                        .build();
-                emitter.send(event).whenComplete(handlerEmitterResponse(KStockPriceProducer.class.getName()));
-            });
+    public void generate(@Observes StartupEvent startupEvent) {
+        vertx.executeBlocking(new Handler<Promise<Void>>() {
+            @Override
+            public void handle(Promise<Void> promise) {
+                while (true) {
+                    IntStream.range(0, BATCH_SIZE).forEach(next -> {
+                        StockPrice event = StockPrice.newBuilder()
+                                .setId("IBM")
+                                .setPrice(random.nextDouble())
+                                .setStatus(status.PENDING)
+                                .build();
+                        emitter.send(event).whenComplete(handlerEmitterResponse(KStockPriceProducer.class.getName()));
+                    });
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+    }
+
+    public void generate2(@Observes StartupEvent startupEvent) {
+        try {
+            Thread.sleep(258);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        vertx.executeBlocking(new Handler<Promise<Void>>() {
+            @Override
+            public void handle(Promise<Void> promise) {
+                while (true) {
+                    IntStream.range(0, BATCH_SIZE).forEach(next -> {
+                        StockPrice event = StockPrice.newBuilder()
+                                .setId("IBM")
+                                .setPrice(random.nextDouble())
+                                .setStatus(status.PENDING)
+                                .build();
+                        emitter.send(event).whenComplete(handlerEmitterResponse(KStockPriceProducer.class.getName()));
+                    });
+                    try {
+                        Thread.sleep(59);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+    }
+
+    public void generate3(@Observes StartupEvent startupEvent) {
+        try {
+            Thread.sleep(185);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        vertx.executeBlocking(new Handler<Promise<Void>>() {
+            @Override
+            public void handle(Promise<Void> promise) {
+                while (true) {
+                    IntStream.range(0, BATCH_SIZE).forEach(next -> {
+                        StockPrice event = StockPrice.newBuilder()
+                                .setId("IBM")
+                                .setPrice(random.nextDouble())
+                                .setStatus(status.PENDING)
+                                .build();
+                        emitter.send(event).whenComplete(handlerEmitterResponse(KStockPriceProducer.class.getName()));
+                    });
+                    try {
+                        Thread.sleep(75);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+    }
+
+    public void generate4(@Observes StartupEvent startupEvent) {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        vertx.executeBlocking(new Handler<Promise<Void>>() {
+            @Override
+            public void handle(Promise<Void> promise) {
+                while (true) {
+                    IntStream.range(0, BATCH_SIZE).forEach(next -> {
+                        StockPrice event = StockPrice.newBuilder()
+                                .setId("IBM")
+                                .setPrice(random.nextDouble())
+                                .setStatus(status.PENDING)
+                                .build();
+                        emitter.send(event).whenComplete(handlerEmitterResponse(KStockPriceProducer.class.getName()));
+                    });
+                    try {
+                        Thread.sleep(358);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
     }
 
     @NotNull
