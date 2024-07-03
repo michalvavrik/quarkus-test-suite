8497750708d6023fede13a688b3fd80e793f805f
Michal Vavřík <mvavrik@redhat.com>
2024-07-03 07:45:35 +0200

    init

---
+++ nosql-db/infinispan/src/main/java/io/quarkus/ts/infinispan/client/RegisterSaslWildflyPackageForReflection.java
@@ -0,0 +1,50 @@
+package io.quarkus.ts.infinispan.client;
+
+import java.security.Provider;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import jakarta.enterprise.event.Observes;
+
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.commons.util.SaslUtils;
+import org.infinispan.commons.util.Util;
+
+import io.quarkus.runtime.StartupEvent;
+
+import javax.security.sasl.SaslClientFactory;
+
+public class RegisterSaslWildflyPackageForReflection {
+
+    private static final Provider[] SECURITY_PROVIDERS;
+
+    void observe(@Observes StartupEvent event) {
+        Collection<SaslClientFactory> clientFactories = SaslUtils
+                .getSaslClientFactories(this.getClass().getClassLoader(), SECURITY_PROVIDERS, true);
+        for (SaslClientFactory clientFactory : clientFactories) {
+            System.out.println("client factory is " + clientFactory);
+        }
+    }
+
+    static {
+        List<Provider> providers = new ArrayList();
+        Iterator var1 = Arrays.asList("org.wildfly.security.sasl.plain.WildFlyElytronSaslPlainProvider",
+                "org.wildfly.security.sasl.digest.WildFlyElytronSaslDigestProvider",
+                "org.wildfly.security.sasl.external.WildFlyElytronSaslExternalProvider",
+                "org.wildfly.security.sasl.oauth2.WildFlyElytronSaslOAuth2Provider",
+                "org.wildfly.security.sasl.scram.WildFlyElytronSaslScramProvider",
+                "org.wildfly.security.sasl.gssapi.WildFlyElytronSaslGssapiProvider",
+                "org.wildfly.security.sasl.gs2.WildFlyElytronSaslGs2Provider").iterator();
+
+        while (var1.hasNext()) {
+            String name = (String) var1.next();
+            Provider provider = (Provider) Util.getInstance(name, RemoteCacheManager.class.getClassLoader());
+            providers.add(provider);
+        }
+
+        SECURITY_PROVIDERS = (Provider[]) providers.toArray(new Provider[0]);
+    }
+}
