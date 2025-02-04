81300c7de69eae6e7a0933c84e81f41f66dd3eb2
Michal Vavřík <mvavrik@redhat.com>
2025-02-04 19:05:00 +0100

    wip16

--- sql-db/vertx-sql/src/test/java/io/quarkus/ts/vertx/sql/handlers/MssqlHandlerIT.java
+++ sql-db/vertx-sql/src/test/java/io/quarkus/ts/vertx/sql/handlers/MssqlHandlerIT.java
@@ -16,8 +16,6 @@ public class MssqlHandlerIT {
 
     @QuarkusApplication
     static final RestService app = new RestService()
-            .withProperty("quarkus.datasource.username", database.getUser())
-            .withProperty("quarkus.datasource.password", database.getPassword())
             .withProperty("quarkus.datasource.jdbc.url", database::getJdbcUrl);
 
     @Test
