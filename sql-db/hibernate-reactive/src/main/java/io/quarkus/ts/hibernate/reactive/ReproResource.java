6ab2eae277bf6becd10ee7a4ed1e2711e37b6b85
Michal Vavřík <mvavrik@redhat.com>
2025-05-22 12:35:59 +0200

    create-table

--- sql-db/hibernate-reactive/src/main/java/io/quarkus/ts/hibernate/reactive/ReproResource.java
+++ sql-db/hibernate-reactive/src/main/java/io/quarkus/ts/hibernate/reactive/ReproResource.java
@@ -1,28 +1,26 @@
 package io.quarkus.ts.hibernate.reactive;
 
-import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.enterprise.event.Observes;
 
-import io.smallrye.mutiny.Uni;
+import io.quarkus.runtime.StartupEvent;
+import io.vertx.ext.web.Router;
 import io.vertx.sqlclient.Pool;
 
-@Path("repro")
 public class ReproResource {
 
-    @Inject
-    Pool pool;
-
-    @GET
-    public Uni<String> queryUsingMysqlClient() {
-        return Uni.createFrom().completionStage(
-                pool
-                        .query("SELECT * FROM authors")
-                        .execute()
-                        .toCompletionStage())
-                .map(Object::toString)
-                .invoke(s -> System.out.println("////// row set is " + s))
-                .onFailure().invoke(t -> System.out.println("///////// failure is " + t));
+    void observe(@Observes StartupEvent event, Pool pool, Router router) {
+        router.route("/repro").handler(context -> pool
+                .query("CREATE TABLE authors (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(31) NOT NULL, PRIMARY KEY(id))")
+                .execute()
+                .onComplete(result -> {
+                    if (result.failed()) {
+                        System.out.println("failed with: " + result.cause());
+                        result.cause().printStackTrace();
+                        context.fail(result.cause());
+                    } else {
+                        context.response().setStatusCode(200).end("Repro result: " + result.result());
+                    }
+                }));
     }
 
 }
