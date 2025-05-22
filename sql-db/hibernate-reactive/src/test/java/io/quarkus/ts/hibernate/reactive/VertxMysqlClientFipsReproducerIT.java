c255644724cc5a7f1efda1bb421d5231cf012e61
Michal Vavřík <mvavrik@redhat.com>
2025-05-22 13:57:13 +0200

    drop-mysql

--- sql-db/hibernate-reactive/src/test/java/io/quarkus/ts/hibernate/reactive/VertxMysqlClientFipsReproducerIT.java
+++ sql-db/hibernate-reactive/src/test/java/io/quarkus/ts/hibernate/reactive/VertxMysqlClientFipsReproducerIT.java
@@ -2,29 +2,16 @@ package io.quarkus.ts.hibernate.reactive;
 
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.test.bootstrap.MySqlService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
-import io.quarkus.test.services.Container;
 import io.quarkus.test.services.QuarkusApplication;
 import io.restassured.filter.log.ResponseLoggingFilter;
 
 @QuarkusScenario
 public class VertxMysqlClientFipsReproducerIT {
 
-    private static final String MYSQL_USER = "quarkus_test";
-    private static final String MYSQL_PASSWORD = "quarkus_test";
-    private static final String MYSQL_DATABASE = "quarkus_test";
-    private static final int MYSQL_PORT = 3306;
-
-    @Container(image = "${mysql.80.image}", port = MYSQL_PORT, expectedLog = "Only MySQL server logs after this point")
-    static MySqlService database = new MySqlService()
-            .withUser(MYSQL_USER)
-            .withPassword(MYSQL_PASSWORD)
-            .withDatabase(MYSQL_DATABASE);
-
     @QuarkusApplication(classes = ReproResource.class)
-    static RestService app = new RestService().withProperty("quarkus.datasource.reactive.url", () -> database.getReactiveUrl());
+    static RestService app = new RestService();
 
     @Test
     public void reproducer() {
