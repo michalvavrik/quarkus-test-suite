686ab6bddb5ff40ae11ae9ef261acb2cd469684d
Michal Vavřík <mvavrik@redhat.com>
2025-05-22 11:14:10 +0200

    debu3

--- security/webauthn/src/main/java/io/quarkus/ts/security/webauthn/security/MyWebAuthnSetup.java
+++ security/webauthn/src/main/java/io/quarkus/ts/security/webauthn/security/MyWebAuthnSetup.java
@@ -20,7 +20,9 @@ public class MyWebAuthnSetup implements WebAuthnUserProvider {
     @WithTransaction
     @Override
     public Uni<List<WebAuthnCredentialRecord>> findByUsername(String username) {
+        System.out.println("//// find by username: " + username);
         return WebAuthnCredential.findByUsername(username)
+                .invoke(list -> System.out.println("////////// find by username list: " + list))
                 .onFailure().invoke(t -> System.out.println("msg iiiiiisssssssssss " + t.getMessage() + " and " + t))
                 .map(list -> list.stream()
                         .map(WebAuthnCredential::toWebAuthnCredentialRecord).toList());
@@ -29,7 +31,10 @@ public class MyWebAuthnSetup implements WebAuthnUserProvider {
     @WithTransaction
     @Override
     public Uni<WebAuthnCredentialRecord> findByCredentialId(String credentialId) {
+        System.out.println("////////// findByCredentialId " + credentialId);
         return WebAuthnCredential.findByCredentialId(credentialId)
+                .invoke(list -> System.out.println("////////// find by username list: " + list))
+                .onFailure().invoke(t -> System.out.println("msg iiiiiisssssssssss " + t.getMessage() + " and " + t))
                 .onItem().ifNull().failWith(() -> new RuntimeException("No such credentials"))
                 .map(WebAuthnCredential::toWebAuthnCredentialRecord);
     }
@@ -37,10 +42,13 @@ public class MyWebAuthnSetup implements WebAuthnUserProvider {
     @WithTransaction
     @Override
     public Uni<Void> store(WebAuthnCredentialRecord credentialRecord) {
+        System.out.println("////////// store " + credentialRecord);
         User newUser = new User();
         newUser.username = credentialRecord.getUsername();
         WebAuthnCredential credential = new WebAuthnCredential(credentialRecord, newUser);
         return credential.persist()
+                .invoke(list -> System.out.println("////////// find by username list: " + list))
+                .onFailure().invoke(t -> System.out.println("msg iiiiiisssssssssss " + t.getMessage() + " and " + t))
                 .flatMap(c -> newUser.persist())
                 .onItem().ignore().andContinueWithNull();
     }
@@ -48,12 +56,16 @@ public class MyWebAuthnSetup implements WebAuthnUserProvider {
     @WithTransaction
     @Override
     public Uni<Void> update(String credentialId, long counter) {
+        System.out.println("////////// update " + credentialId);
         return WebAuthnCredential.findByCredentialId(credentialId)
+                .invoke(list -> System.out.println("////////// find by username list: " + list))
+                .onFailure().invoke(t -> System.out.println("msg iiiiiisssssssssss " + t.getMessage() + " and " + t))
                 .onItem().ignore().andContinueWithNull();
     }
 
     @Override
     public Set<String> getRoles(String userId) {
+        System.out.println("////////// get roles " + userId);
         if (userId.equals("admin")) {
             Set<String> ret = new HashSet<>();
             ret.add("user");
