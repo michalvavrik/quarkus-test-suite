4f81bb9e93197ef056b92f250a70fb451ea0b324
Michal Vavřík <mvavrik@redhat.com>
2025-08-16 02:43:44 +0200

    wip

---
+++ monitoring/micrometer-opentelemetry/src/test/java/io/quarkus/ts/monitoring/micrometeropentelemetry/test/MicrometerOpenTelemetryBridgeIT.java
@@ -0,0 +1,33 @@
+package io.quarkus.ts.monitoring.micrometeropentelemetry.test;
+
+import io.quarkus.test.bootstrap.GrafanaService;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.GrafanaContainer;
+import io.quarkus.test.services.QuarkusApplication;
+
+@QuarkusScenario
+public class MicrometerOpenTelemetryBridgeIT extends AbstractMicrometerOpenTelemetryBridgeIT {
+
+    @GrafanaContainer
+    static final GrafanaService grafana = new GrafanaService();
+
+    @QuarkusApplication
+    static final RestService app = new RestService()
+            .withProperty("otlp-collector-url", grafana::getOtlpCollectorUrl);
+
+    @Override
+    protected String getLokiUrl() {
+        return grafana.getLokiUrl();
+    }
+
+    @Override
+    protected String getTempoUrl() {
+        return grafana.getTempoUrl();
+    }
+
+    @Override
+    protected String getPrometheusUrl() {
+        return grafana.getPrometheusUrl();
+    }
+}
